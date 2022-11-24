a830c7954603e636aa287723e8a32f5dca3b3435
yinhsuan <sandy10235168@gmail.com>
2022-11-24 23:55:03 +0800

    edit pi_block_linear.cc and become faster

--- src/pi_block_linear.cc
+++ src/pi_block_linear.cc
@@ -15,51 +15,45 @@ int main(int argc, char **argv)
     int world_rank, world_size;
     // ---
 
-    static unsigned int seed = world_rank;
-    long long number_in_circle = 0;
-    long long count = tosses / world_size;
-    long long remain = tosses % world_size;
-    double x, y, distance_squared;
-    MPI_Status status;
-
     // TODO: init MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // find out process rank
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // find out numbe of process
 
+    static unsigned int seed = world_rank;
+    long long int number_in_circle = 0;
+    long long int count = tosses / world_size;
+    long long int remain = tosses % world_size;
+    long long int local_number = 0;
+    double x, y, distance_squared;
+    MPI_Status status;
+
+    for (int toss = 0; toss<count; toss++) {
+        x = (double) rand_r(&seed)/RAND_MAX;
+        y = (double) rand_r(&seed)/RAND_MAX;
+        if (x * x + y * y <= 1) {
+            local_number++;
+        }
+    }
+
     if (world_rank > 0)
     {
         // TODO: handle workers
-        for ( int toss = 0; toss < count; toss++) {
-            x = (double) rand_r(&seed)/RAND_MAX;
-            y = (double) rand_r(&seed)/RAND_MAX;
-            distance_squared = x * x + y * y;
-            if (distance_squared <= 1) {
-                number_in_circle++;
-            }
-        }
-        MPI_Send(&number_in_circle, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&local_number, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: master
-        for ( int toss = 0; toss < (count+remain); toss++) {
-            x = (double) rand_r(&seed)/RAND_MAX;
-            y = (double) rand_r(&seed)/RAND_MAX;
-            distance_squared = x * x + y * y;
-            if (distance_squared <= 1) {
-                number_in_circle++;
-            }
+        number_in_circle = local_number;
+        long long int buf = 0;
+        for (int worker=1; worker<world_size; worker++) {
+            MPI_Recv(&buf, 1, MPI_LONG_LONG, worker, 0, MPI_COMM_WORLD, &status);
+            number_in_circle += buf;
         }
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
-        long long local_number = 0;
-        for (int worker=1; worker<world_size; worker++) {
-            MPI_Recv(&local_number, 1, MPI_LONG_LONG, worker, 0, MPI_COMM_WORLD, &status);
-            number_in_circle += local_number;
-        }
         pi_result = 4 * number_in_circle /((double) tosses);
 
         // --- DON'T TOUCH ---

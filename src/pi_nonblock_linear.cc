7f5d0b608dfe8cbc17f2ad7665d94005f4dd9346
yinhsuan <sandy10235168@gmail.com>
2022-11-26 00:07:51 +0800

    finish pi_nonblock_linear.cc

--- src/pi_nonblock_linear.cc
+++ src/pi_nonblock_linear.cc
@@ -16,23 +16,51 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    static unsigned int seed = world_rank;
+    long long int number_in_circle = 0;
+    long long int count = tosses / world_size;
+    long long int remain = tosses % world_size;
+    long long int local_number = 0;
+    double x, y;
+    // MPI_Status status;
+
+    count = (world_rank == 0) ? (count + remain) : count;
+    for (int toss = 0; toss<count; toss++) {
+        x = (double) rand_r(&seed)/RAND_MAX;
+        y = (double) rand_r(&seed)/RAND_MAX;
+        if (x * x + y * y <= 1) {
+            local_number++;
+        }
+    }
 
     if (world_rank > 0)
     {
         // TODO: MPI workers
+        MPI_Request request;
+        MPI_Isend(&local_number, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD, &request);
     }
     else if (world_rank == 0)
     {
         // TODO: non-blocking MPI communication.
         // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
-        MPI_Request requests[];
-
-        MPI_Waitall();
+        MPI_Request requests[world_size - 1];
+        MPI_Status statuses[world_size - 1];
+        long long int buf;
+        for (int src=1; src<world_size; src++) {
+            MPI_Irecv(&buf, 1, MPI_LONG_LONG, src, 0, MPI_COMM_WORLD, &requests[src-1]);
+            local_number += buf;
+        }
+        MPI_Waitall(world_size - 1, requests, statuses);
     }
 
     if (world_rank == 0)
     {
         // TODO: PI result
+        number_in_circle = local_number;
+        pi_result = 4 * number_in_circle /((double) tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();

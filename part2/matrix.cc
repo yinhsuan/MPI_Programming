411046e6e4dfe9b89b6439c74f251e9ce8f1f238
yinhsuan <sandy10235168@gmail.com>
2022-11-26 21:58:22 +0800

    finish matrix multiplicaiton

--- part2/matrix.cc
+++ part2/matrix.cc
@@ -14,7 +14,6 @@
 void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
                         int **a_mat_ptr, int **b_mat_ptr) {
     int world_rank, world_size;
-    int n, m. l;
     int *ptr;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -22,9 +21,9 @@ void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
 
     if (world_rank == MASTER) {
         scanf("%d %d %d", n_ptr, m_ptr, l_ptr);
-        n = *n_ptr;
-        m = *m_ptr;
-        l = *l_ptr;
+        int n = *n_ptr;
+        int m = *m_ptr;
+        int l = *l_ptr;
 
         *a_mat_ptr = (int*)calloc(n * m, sizeof(int));
 	    *b_mat_ptr = (int*)calloc(m * l, sizeof(int));
@@ -50,10 +49,54 @@ void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
 // b_mat: a continuous memory placing m * l elements of int
 void matrix_multiply(const int n, const int m, const int l,
                      const int *a_mat, const int *b_mat) {
-    
+    int world_rank, world_size;
+    int start_row, end_row;
+    int count = n / world_size;
+    int sum;
+    int *C, *ans;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    C = (int*) calloc(n * l, sizeof(int));
+	ans = (int*) calloc(n * l, sizeof(int));
+
+    start_row = count * world_rank;
+    end_row = (world_rank == world_size-1) ? n : (start_row + count);
+
+    for (int i=start_row; i<end_row; i++) {
+        for (int j=0; j<l; j++) {
+            sum = 0;
+            for (int k=0; k<m; k++) {
+                sum += a_mat[i*m + k] * b_mat[j + k*l];
+            }
+            C[i*l + j] = sum;
+        }
+    }
+    MPI_Reduce(C, ans, (n * l), MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(world_rank == MASTER){
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<l; j++) {
+                if (j) {
+                    // printf(" ");
+                    putchar(' ');
+                }
+                printf("%d", ans[i*l + j]);
+            }
+            // printf("\n");
+            putchar('\n');
+        }
+	}
 }
 
 // Remember to release your allocated memory
 void destruct_matrices(int *a_mat, int *b_mat) {
-    
+    int world_rank, world_size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	if (world_rank == MASTER) {
+		free(a_mat);
+		free(b_mat);
+	}
 }

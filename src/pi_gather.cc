d1fb77a7eaf1c596376a172bc6308d1d94aa7802
yinhsuan <sandy10235168@gmail.com>
2022-11-26 10:52:34 +0800

    finish pi_gather.cc

--- src/pi_gather.cc
+++ src/pi_gather.cc
@@ -16,12 +16,40 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    static unsigned int seed = world_rank;
+    long long int number_in_circle = 0;
+    long long int count = tosses / world_size;
+    long long int remain = tosses % world_size;
+    long long int local_number = 0;
+    long long int *recvbuf;
+    double x, y;
+
+    count = (world_rank == 0) ? (count + remain) : count;
+    for (int toss = 0; toss<count; toss++) {
+        x = (double) rand_r(&seed)/RAND_MAX;
+        y = (double) rand_r(&seed)/RAND_MAX;
+        if (x * x + y * y <= 1) {
+            local_number++;
+        }
+    }
 
     // TODO: use MPI_Gather
+    if (world_rank == 0) {
+        recvbuf = (long long int *) malloc(world_size * sizeof(long long int));
+    }
+    MPI_Gather(&local_number, 1, MPI_LONG_LONG, recvbuf, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
     {
         // TODO: PI result
+        for (int i=1; i<world_size; i++) {
+            local_number += recvbuf[i];
+        }
+        number_in_circle = local_number;
+        pi_result = 4 * number_in_circle /((double) tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
